8f632631ef4226f756910b0e385603b5684d49e6
Rakesh M <rakeshnitcalicut@gmail.com>
2015-10-23 00:09:09 +0530

    bootstrap practice

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,46 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+		//Do this when the page loags
+		//Add customer option is loaded by default
+		$("#getCustomerForm").hide();
+		$("#getAccountForm").hide();
+		$("#addAccountForm").hide();
+		$("#getAllAccounts").hide();
+		$("#getTransactionList").hide();
+
+		//OnClick Events to execute the features
+
+		$("#oAddCustomer").click(
+				function(){
+					$(".well").fadeOut();
+					$("#addCustomerForm").fadeIn();
+				}
+			);
+
+		$("#oGetCustomer").click(
+				function(){
+					$(".well").fadeOut(function(){$("#getCustomerForm").fadeIn();});
+				}
+			);
+
+		$("#oGetAccount").click(
+				function(){
+					$(".well").fadeOut();
+					$("#getAccountForm").fadeIn();
+				}
+			);
+
+		$("#oGetAllAccounts").click(
+				function(){
+					$(".well").fadeOut();
+					$("#getAllAccounts").fadeIn();
+				}
+			);
+
+		$("#oGetTransactionList").click(
+				function(){
+					$(".well").fadeOut();
+					$("#getTransactionList").fadeIn();
+				}
+			);		
+
+	});
